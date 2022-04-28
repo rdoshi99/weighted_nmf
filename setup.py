1c38f95605146ab668b5cd52147387e73dbf5348
rdoshi99 <46800082+rdoshi99@users.noreply.github.com>
2022-04-28 12:31:34 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="weighted_nmf",
+    version="0.0.1",
+    author="Ruhi Doshi",
+    author_email="rdoshi99@berkeley.edu",
+    description="Variance-weighted non-negative matrix factorization package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rdoshi99/weighted_nmf",
+    packages=['weighted_nmf']
+    install_requires=['numpy', 'tqdm']
+)
